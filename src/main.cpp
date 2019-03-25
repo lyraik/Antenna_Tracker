3e8acac24952edd3c9347cf216d9c97387cde20b
Dominik Gschwind <dominik.gschwind99@gmail.com>
2019-03-25 13:21:50 +0100

    Restored sdkconfig and main.cpp. Improved platformio.ini

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file main.cpp
+ * @author Dominik Gschwind (dominik.gschwind99@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-03-25
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+/**
+ * @brief Entrypoint of application.
+ */
+extern "C" void app_main()
+{
+}
