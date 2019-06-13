1bb26fcc93cb55201b53ea61b356e2eae0783eff
MorpheusJ <43730356+MorpheusJ@users.noreply.github.com>
2019-06-13 22:16:11 +0200

    GPS- Funktionen
    übernehmen von funktionen

--- src/GPS/GPS.h
+++ src/GPS/GPS.h
@@ -1,5 +1,17 @@
 //Jonas
 #include <iostream>
+#include "GPS.h"
+
+
+#include <stdint.h>
+#include "freertos/FreeTROS.h"
+#include "freetros/task.h"
+#include "stdlib.h"
+#include "ctype.h"
+#include "math.h"
+#include "esp_log.h"
+
+#include "minmea.h"
 
 /**
  * @brief 
@@ -8,6 +20,7 @@
  */
 namespace GPS
 {
+char *readLine(uart_port_t uart);
 void init(uint8_t TxPin, uint8_t RxPin);
 float getLong();
 float getLat();
